b26eda91a204edcc095dc06d99617ccdfa3e644b
Enderson Menezes (Mr. Enderson) <endersonster@gmail.com>
2021-09-17 19:52:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    numero1 = request.args.get('numero1', None)
-    numero2 = request.args.get('numero2', None)
+@app.route("/api/calcular")
+def api_calcular():
+    numero1 = request.form.get('numero1', None)
+    numero2 = request.form.get('numero2', None)
     try:
         int(numero1)
     except ValueError:
@@ -19,6 +19,12 @@ def hello_world():
     else:
         resultado = "Envie dois numeros para somar."
     return jsonify({'resultado': resultado})
+
+@app.route("/")
+def hello_world():
+    data = {
+    }
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
 	app.run(debug=True)
